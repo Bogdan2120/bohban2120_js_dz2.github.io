be01648967548192ed731defb733c1d6fff9927e
Bohdan Vasylyk <bogdanvasilik2120@gmail.com>
2017-07-24 12:13:30 +0300

    homework_2

--- 3/js.js
+++ 3/js.js
@@ -1,13 +1,67 @@
-const numb = document.querySelector('input[type="text"]').value;
-const button = document.querySelector("button");
-button.addEventListener("click", numEntrance);
-
-function numEntrance() {
-    if (numb <= 20) {
-        console.log ('1-й подъезд');
-    } else if (numb <= 64) {
-        console.log ('2-й подъезд');
-    } else if (numb <= 80) {
-        console.log ('3-й подъезд');
-    }
+function showResult(text) {
+    console.log(text);
 }
+
+let house = {
+    numb : document.querySelector('input[type="text"]').value,
+    button : document.querySelector("button"),
+    porches : 3,
+    porch1 : {
+        from : 1,
+        to : 20
+    },
+    porch2 : {
+        from : 21,
+        to : 65
+    },
+    porch3 : {
+        from : 66,
+        to : 80
+    },
+    error : 'В доме нет такой квартиры',
+    info :  'Эта квартира в подъезде № '
+}
+
+function numbPorch(number, obj) {
+    let result = obj.error;
+
+    for (var i = obj.porches; i > 0; i++) {
+        if (number >= obj["porch" + i].from && number <= obj["porch" + i].from) {
+            result = obj.info + i;
+        }
+    }
+    return result;
+}
+house.button.addEventListener('click', setPorch);
+
+function setPorch() {
+    let result = numbPorch(house.numb, house);
+    showResult(result);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const numb = document.querySelector('input[type="text"]').value;
+// const button = document.querySelector("button");
+// button.addEventListener("click", numEntrance);
+
+// function numEntrance() {
+//     if (numb <= 20) {
+//         console.log ('1-й подъезд');
+//     } else if (numb <= 64) {
+//         console.log ('2-й подъезд');
+//     } else if (numb <= 80) {
+//         console.log ('3-й подъезд');
+//     }
+// }
--- for/js.js
+++ for/js.js
@@ -1,52 +1,76 @@
+function showResult(text) {
+    console.log(text);
+}
 //dz 1
-// for (var i = 0; i < 101; i++) {
-//     if (i % 2) {
-//         continue;
-//     } else {
-//         console.log (i);
-//     }
-// }
-
+console.log('==============DZ1==================');
+let evens = '';
+for (var i = 0; i < 101; i = i + 2) {
+   evens += i + ',';
+}
+showResult(evens);
+console.log('===============DZ2=================');
 //dz2
-// for (var i = 200; i >= 0 ; i--) {
-//     console.log (i);
-// }
-
+let evens1 = '';
+for (var i = 200; i >= 0 ; i--) {
+       evens1 += i + ',';
+}
+showResult(evens1);
+  
+console.log('===============DZ3======================');
 
 //dz3
-// for (var i = 0; i <= 100; i++) {
-//     let num = i 
-//     let sum1  = num + i;
-//     console.log (sum1);
-// }
+let sum = 0;
+let evens2 = '';
+for (var i = 0; i <= 100; i++) {
+    let res = sum += i;
+    evens2 += res + ',';
+}
+showResult(evens2);
 
-//dz4 не смог разобраться
-// const num = document.querySelector('.num').value;
-// const st = document.querySelector('.st').value;
-// const butt = document.querySelector('button');
-// butt.addEventListener('click', addSt)
-// function addSt() {
-//     for (var i = num; i < 100 ;i * i) {
-//     console.log(i);
-// }
-// }
+//dz4 
+
+const num = document.querySelector('.num').value;
+const exponent = document.querySelector('.st').value;
+const butt = document.querySelector('button');
+butt.addEventListener('click', addSt); 
+let pow = num;
+let evens3 = '';
+function addSt() {
+    for (var i = 1; i < exponent ; i++) {
+      let res = pow *= num;
+      evens3 += res + ',';
+}
+    console.log('===============DZ4======================');
+    showResult(evens3);
+}
 
 
+console.log('===============DZ5======================');
 
 // dz5 
-// for (var i = 0; i < 10; i++) {
-//     let res = i * 7;
-//     console.log(i + ' * 7 = ' + res )
-// }
+for (var i = 0; i < 10; i++) {
+    console.log(i + ' * 7 = ' + i * 7)
+}
+
+
+console.log('===============DZ6======================');
 
 // dz 6
-// for (var i = 0; i <= 50; i++) {
-//     console.log(i);
-// }
-
+let evens4 = '';
+for (var i = 0; i <= 50; i++) {
+    evens4 += i + ',';
+}
+showResult(evens4);
 
 // dz7
-for (var i = 1000; i < 2000; i++) {
-    console.log('&#' + i);
-    
+const parent = document.querySelector('div');
+function showResultInDOM(parentDOM, tagString, text) {
+   let elem = document.createElement(tagString);
+   elem.textContent = text;
+   parentDOM.appendChild(elem);
 }
+var res = "";
+for (var i = 1000; i < 2000; i++) {
+    res += '&#' + i + '; ';
+} 
+showResultInDOM(parent, 'p', res);
